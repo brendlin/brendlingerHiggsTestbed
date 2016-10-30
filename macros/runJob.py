3b9bfd8f442faf57a65ccf24451ebf842c4741a5
Kurt Brendlinger <brendlik@nafhh-atlas01.desy.de>
2016-10-30 20:48:24 +0100

    Clean up runJob

--- macros/runJob.py
+++ macros/runJob.py
@@ -69,14 +69,32 @@ def main (options,args) :
 
     myjob = ROOT.EL.Job()
     myjob.options().setString (ROOT.EL.Job.optXaodAccessMode, ROOT.EL.Job.optXaodAccessMode_branch)
-    alg = ROOT.TutorialAnalysis("TutorialAnalysis")
+    exec ('alg = ROOT.%s("%s")'%(options.alg,options.alg))
     conf = ROOT.HG.Config()
     conf.addFile(options.config)
+
+    # TEnv uses value from first file it's specified in.
+    # If specified, read in additional configuration
+    if (conf.isDefined("Include")) :
+        for cfg in conf.getStrV("Include") :
+            conf.addFile(cfg);
+
+    # Fill unspecified values from default config, specified here.
+    if not conf.isDefined("BaseConfig") :
+        print 'You must specify a base configuration file, option: BaseConfig. Exiting.'
+        import sys; sys.exit()
     conf.addFile(ROOT.PathResolverFindCalibFile(conf.getStr("BaseConfig").Data()))
+
     conf.setValue('OutputDir',outputname)
     alg.setConfig(conf);
     myjob.algsAdd(alg)
     myjob.options().setDouble(ROOT.EL.Job.optMaxEvents,options.n)
+
+    # RunUtils has:
+    # InputFileList from conf (uses SH::readFileList)
+    # addGrid (InputFaxDS)
+    # files from argv command line
+    # (could also do scanDir or whatever)
 
     fileName = options.input # '/Users/brendlin/Temporary/data16_13TeV.DS1_2496ipb.physics_Main.MxAOD.p2667.h013a.root'
     myhandler = ROOT.SH.SampleHandler()
@@ -108,41 +126,42 @@ if __name__ == "__main__":
     # Options to input data. isData and isAtlfast are only used when --input is specified.
     #
     p.add_option('-i','--input',type  ='string'     ,default=''   ,dest='input'  ,help='input directory (as ready by SH)')
-    p.add_option('--isData'   ,action='store_true',default=False,dest='isData'   ,help='IsData'   )
-    p.add_option('--isAtlfast',action='store_true',default=False,dest='isAtlfast',help='isAtlfast')
-    p.add_option('--mc15a'    ,action='store_true',default=False,dest='mc15a'    ,help='(for pileup reweighting) is it mc15a? Default is mc15c.')
-    p.add_option('--mc15b'    ,action='store_true',default=False,dest='mc15b'    ,help='(for pileup reweighting) is it mc15a? Default is mc15c.')
+    # p.add_option('--isData'   ,action='store_true',default=False,dest='isData'   ,help='IsData'   )
+    # p.add_option('--isAtlfast',action='store_true',default=False,dest='isAtlfast',help='isAtlfast')
+    # p.add_option('--mc15a'    ,action='store_true',default=False,dest='mc15a'    ,help='(for pileup reweighting) is it mc15a? Default is mc15c.')
+    # p.add_option('--mc15b'    ,action='store_true',default=False,dest='mc15b'    ,help='(for pileup reweighting) is it mc15a? Default is mc15c.')
 
-    p.add_option('--samples'   ,type  ='string'     ,default=''   ,dest='samples',help='input samples (see Sample.h)')
+    # p.add_option('--samples'  ,type  ='string'     ,default=''   ,dest='samples',help='input samples (see Sample.h)')
+    p.add_option('--alg'      ,type  ='string'     ,default=''   ,dest='alg'    ,help='algorithm (SkimAndSlim, HGamCutflowAndMxAOD, ...')
     #
     # Other options
     #
-    p.add_option('-n','--n',type  ='int'        ,default=-1   ,dest='n'     ,help='max nevents to run over'  )
-    p.add_option('--max'  ,type  ='int'        ,default=500000,dest='max'   ,help='max nevents per condor process'  )
-    p.add_option('--dconfig',type  ='string',default='DefaultConfiguration_mc15.conf',dest='dconfig',help='default config file' )
+    p.add_option('-n','--n',type  ='int'        ,default=-1    ,dest='n'     ,help='max nevents to run over'  )
+    p.add_option('--max'   ,type  ='int'        ,default=500000,dest='max'   ,help='max nevents per condor/sge process'  )
+    # p.add_option('--dconfig',type  ='string',default='DefaultConfiguration_mc15.conf',dest='dconfig',help='default config file' )
     p.add_option('--config' ,type  ='string',default='sr3l_soft.conf'           ,dest='config' ,help='config file' )
     p.add_option('--out'   ,type  ='string'    ,default='out'   ,dest='out',help='output name' )
-    p.add_option('--treename'  ,type  ='string',default='susy'  ,dest='treename',help='tree name' )
-    p.add_option('--skim'  ,action='store_true',default=False  ,dest='skim',help='do a skim (d3pd)' )
-    p.add_option('--grid'  ,action='store_true',default=False  ,dest='grid',help='send to grid (d3pd)' )
-    p.add_option('--submit',action='store_true',default=False  ,dest='submit',help='Submit to grid (d3pd)' )
-    p.add_option('--ntuple',action='store_true',default=False  ,dest='ntuple',help='write an ntuple' )
-    p.add_option('--condor',action='store_true',default=False  ,dest='condor',help='use condor' )
+    # p.add_option('--treename'  ,type  ='string',default='susy'  ,dest='treename',help='tree name' )
+    # p.add_option('--skim'  ,action='store_true',default=False  ,dest='skim',help='do a skim (d3pd)' )
+    # p.add_option('--grid'  ,action='store_true',default=False  ,dest='grid',help='send to grid (d3pd)' )
+    # p.add_option('--submit',action='store_true',default=False  ,dest='submit',help='Submit to grid (d3pd)' )
+    # p.add_option('--ntuple',action='store_true',default=False  ,dest='ntuple',help='write an ntuple' )
+    # p.add_option('--condor',action='store_true',default=False  ,dest='condor',help='use condor' )
     p.add_option('--sge',action='store_true',default=False  ,dest='sge',help='use SGE driver' )
-    p.add_option('--gridsamples',type ='string',default='GridSamples.txt',dest='gridsamples',help='Grid samples text file' )
-    p.add_option('--tag',type  ='string'    ,default='dc14_ew01'   ,dest='tag',help='dataset tag' )
-    p.add_option('--truth'  ,action='store_true',default=False  ,dest='truth',help='truth level analysis on truth DAODs' )
-    p.add_option('--susy'  ,action='store_true',default=False  ,dest='susy',help='Use susy object selection (instead of WZ object selection)' )
-    p.add_option('--susy2016'  ,action='store_true',default=False  ,dest='susy2016',help='Use susy2016 object selection (instead of WZ object selection)' )
-    p.add_option('--systematics'  ,action='store_true',default=False  ,dest='systematics',help='Run all systematic variations' )
+    # p.add_option('--gridsamples',type ='string',default='GridSamples.txt',dest='gridsamples',help='Grid samples text file' )
+    # p.add_option('--tag',type  ='string'    ,default='dc14_ew01'   ,dest='tag',help='dataset tag' )
+    # p.add_option('--truth'  ,action='store_true',default=False  ,dest='truth',help='truth level analysis on truth DAODs' )
+    # p.add_option('--susy'  ,action='store_true',default=False  ,dest='susy',help='Use susy object selection (instead of WZ object selection)' )
+    # p.add_option('--susy2016'  ,action='store_true',default=False  ,dest='susy2016',help='Use susy2016 object selection (instead of WZ object selection)' )
+    # p.add_option('--systematics'  ,action='store_true',default=False  ,dest='systematics',help='Run all systematic variations' )
 
     options,args = p.parse_args()
-    if options.grid :
-       options.skim = True
+    # if options.grid :
+    #    options.skim = True
 
-    if options.systematics and not options.condor :
-        print 'You are trying to run all systematics locally. Please use --condor.'
-        import sys
-        sys.exit()
+    # if options.systematics and not options.condor :
+    #     print 'You are trying to run all systematics locally. Please use --condor.'
+    #     import sys
+    #     sys.exit()
 
     main(options,args)
